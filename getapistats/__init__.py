56a54f0c85af1efa4710c440909c3bc22802a912
edward <edward@hilgendorf.me>
2022-06-12 20:32:07 -0700

    cleanup

--- getapistats/__init__.py
+++ getapistats/__init__.py
@@ -14,8 +14,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             json_data = json.dumps(get_test_data.test_data)
         except Exception as e:
             logging.info("Bad JSON: " + e )
-            func.HttpResponse("Bad JSON: " + repr(e))
     except Exception as e:
         logging.debug(e)
-        return func.HttpResponse("Error, see logs!" + repr(e), status_code=200)
+        return func.HttpResponse("Error, see logs" + repr(e), status_code=200)
     return func.HttpResponse(str(json_data), status_code=200)
